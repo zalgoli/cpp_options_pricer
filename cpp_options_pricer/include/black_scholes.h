2b160221981e30a4a19bbdaee8418fa39228dc3f
Zoltan Gal <galzoltan.jr@gmail.com>
2025-11-18 18:49:12 +0100

    Augmented Black-Scholes logic

--- cpp_options_pricer/include/black_scholes.h
+++ cpp_options_pricer/include/black_scholes.h
@@ -2,6 +2,9 @@
 #ifndef NORMCDF_H
 #define NORMCDF_H
 
+inline double computeD1(double S, double K, double r, double sigma, double T);
+inline double computeD2(double S, double K, double r, double sigma, double T);
+
 double bs_call(double S, double K, double r, double sigma, double T);
 double bs_put(double S, double K, double r, double sigma, double T);
 
--- cpp_options_pricer/src/black_scholes.cpp
+++ cpp_options_pricer/src/black_scholes.cpp
@@ -1,12 +1,37 @@
 #include "black_scholes.h"
 #include "normcdf.h"
+#include <cmath>
+
+/*
+S - Spot
+K - Strike
+r - risk-free rate
+sigma - volatility
+T - time to maturity
+*/
+
+inline double computeD1 (double S, double K, double r, double sigma, double T)
+{
+	return ((std::log(S / K)) + (r + 0.5 * sigma * sigma, 2) * T) / (sigma * std::sqrt(T));
+}
+
+inline double computeD2(double S, double K, double r, double sigma, double T)
+{
+	return computeD1(S, K, r, sigma, T) - (sigma * std::sqrt(T));
+}
 
 double bs_call(double S, double K, double r, double sigma, double T)
 {
-	return 0.0;
+	double d1 = computeD1(S, K, r, sigma, T);
+	double d2 = computeD2(S, K, r, sigma, T);
+
+	return (S * normcdf(d1)) - (K * std::exp(-r * T) * normcdf(d2));
 }
 
 double bs_put(double S, double K, double r, double sigma, double T)
 {
-	return 0.0;
+	double d1 = computeD1(S, K, r, sigma, T);
+	double d2 = computeD2(S, K, r, sigma, T);
+
+	return (K * std::exp(-r * T) * normcdf(-d2)) - (S * normcdf(-d1));
 }
